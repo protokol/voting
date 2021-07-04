40dcca24afb8f710444844e28e8532e8c21b4506
KovacZan <zan@protokol.com>
2021-07-04 10:11:45 +0200

    documentation(voting-crypto): add comment

--- packages/voting-crypto/src/index.ts
+++ packages/voting-crypto/src/index.ts
@@ -5,6 +5,7 @@ import * as Interfaces from "./interfaces";
 import * as Transactions from "./transactions";
 import * as Utils from "./utils";
 
+// Register keyword if this module is loaded
 Utils.castVoteOptionValidator("votingSchema");
 
 export { Builders, Defaults, Enums, Interfaces, Transactions, Utils };
