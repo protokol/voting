01c56c400e5cfc98ee94174e91e59b88e446f4d5
KovacZan <zan@protokol.com>
2021-07-04 08:38:36 +0200

    feat(voting-crypto): implement cast vote ser/deser and schema

--- packages/voting-crypto/src/transactions/cast-vote.ts
+++ packages/voting-crypto/src/transactions/cast-vote.ts
@@ -1,7 +1,9 @@
 import { Utils } from "@arkecosystem/crypto";
+import { Asserts } from "@protokol/utils";
 import ByteBuffer from "bytebuffer";
 
 import { VotingStaticFees, VotingTransactionTypes } from "../enums";
+import { ICastVote } from "../interfaces";
 import { AbstractVotingTransaction } from "./abstract-transaction";
 
 export class CastVoteTransaction extends AbstractVotingTransaction {
@@ -18,20 +20,52 @@ export class CastVoteTransaction extends AbstractVotingTransaction {
 				votingCastVote: {
 					type: "object",
 					required: ["proposalId", "decision"],
+					properties: {
+						proposalId: {
+							$ref: "transactionId",
+						},
+						decision: {
+							type: "string",
+							votingSchema: true,
+						},
+					},
 				},
 			},
 		};
 	}
 
 	public serialize(): ByteBuffer {
-		return new ByteBuffer(0);
+		const { data } = this;
+
+		Asserts.assert.defined<ICastVote>(data.asset?.votingCastVote);
+
+		const castVoteAsset: ICastVote = data.asset.votingCastVote;
+
+		const decisionBuffer: Buffer = Buffer.from(castVoteAsset.decision);
+
+		const buffer: ByteBuffer = new ByteBuffer(32 + decisionBuffer.length, true);
+
+		buffer.append(castVoteAsset.proposalId);
+
+		buffer.writeUint8(decisionBuffer.length);
+		buffer.append(castVoteAsset.decision);
+
+		return buffer;
 	}
 
 	public deserialize(buf: ByteBuffer): void {
 		const { data } = this;
 
+		const proposalId = buf.readBytes(32).toString("hex");
+
+		const decisionLength: number = buf.readUInt8();
+		const decision = buf.readBytes(decisionLength).toString("hex");
+
 		data.asset = {
-			votingCastVote: {},
+			votingCastVote: {
+				proposalId,
+				decision,
+			},
 		};
 	}
 }
